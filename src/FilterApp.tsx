6e5939c698bb705d69c3a83bae1eadae2236b52b
CTZDev <carlosteranzavaleta@hotmail.com>
2024-10-16 21:24:45 -0500

    (feat) Add layout with components

--- src/FilterApp.tsx
+++ src/FilterApp.tsx
@@ -1,15 +1,14 @@
-import Filter from "./components/Filter";
-import optionsFilter from "./consts/filters";
+import { Filters } from "./components/Filters";
+import { Header } from "./components/Header";
+import { Table } from "./components/Table";
 import "./FilterApp.css";
 
 function FilterApp() {
   return (
     <div className="min-h-dvh">
-      <h1 className="text-center font-semibold text-4xl p-8">
-        Buscador de Autos
-      </h1>
-
-      <Filter data={optionsFilter[0].Marca?.options} />
+      <Header title="Buscador de autos" />
+      <Filters />
+      <Table />
     </div>
   );
 }

e60d3995452ae72fa9cfbf2e46d58ce6c1c9f2b1
CTZDev <carlosteranzavaleta@hotmail.com>
2024-10-16 21:23:34 -0500

    (feat) Add type of Car

--- src/types/Filter.ts
+++ src/types/Filter.ts
@@ -1,18 +1,14 @@
-type Option = {
+type OptionCar = {
   value: string;
   label: string;
 };
 
-type FilterOption = {
-  options: Option[];
-};
-
-export type Filters = {
-  Marca?: FilterOption;
-  Año?: FilterOption;
-  "Precio min"?: FilterOption;
-  "Precio max"?: FilterOption;
-  Puertas?: FilterOption;
-  Transmisión?: FilterOption;
-  Color?: FilterOption;
+export type Car = {
+  Marca: Array<OptionCar>;
+  Anio: Array<OptionCar>;
+  "Precio min": Array<OptionCar>;
+  "Precio max": Array<OptionCar>;
+  Puertas: Array<OptionCar>;
+  Transmisión: Array<OptionCar>;
+  Color: Array<OptionCar>;
 };

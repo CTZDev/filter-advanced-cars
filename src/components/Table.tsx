3c3e85befa3b059250f65a07e3d57c921d292c38
CTZDev <carlosteranzavaleta@hotmail.com>
2024-10-17 21:55:52 -0500

    (feat) Add results in table

--- src/components/Table.tsx
+++ src/components/Table.tsx
@@ -1,33 +1,42 @@
 import type React from "react";
-import dataCar from "../consts/dataCar";
+import { useContext } from "react";
+import { CarContext } from "../context/carContext";
+import type { CarList } from "../types/CarList";
 
-const convertCar = dataCar.map((car) => {
-  const brand = car.brand.toUpperCase().replace(" ", "");
-  const year = car.year;
-  const price = car.price;
-  const doors = car.doors;
-  const transmission = car.transmission.toUpperCase();
-  const color = car.color.toUpperCase();
-  return [brand, year, price, doors, transmission, color].join(" - ");
-});
+const updateCars = (carList: Array<CarList>) => {
+  const cars = carList.map((car) => {
+    const brand = car.brand.toUpperCase();
+    const year = car.year;
+    const price = car.price;
+    const doors = car.doors;
+    const transmission = car.transmission.toUpperCase();
+    const color = car.color.toUpperCase();
+    return [
+      brand,
+      year,
+      doors + " PUERTAS",
+      "TRANSMISIÓN: " + transmission,
+      "PRECIO: " + price,
+      "COLOR: " + color,
+    ].join(" - ");
+  });
+  return cars;
+};
 
 export const Table: React.FC = () => {
+  const { carsFilter } = useContext(CarContext);
+  const listOfCars = updateCars(carsFilter);
+  console.log(listOfCars);
+
   return (
     <>
-      <h2 className="font-bold text-pretty text-center text-3xl p-8">
-        Resultados
-      </h2>
+      <h2 className="font-bold text-pretty text-center text-3xl p-8">RESULTADOS</h2>
       <ul>
-        {convertCar.map((data) => (
-          <>
-            <li
-              key={data}
-              className="text-center text-base p-1.5"
-            >
-              {data}
-            </li>
+        {listOfCars.map((data) => (
+          <li key={data} className="text-center text-base p-1.5 font-medium">
+            {data}
             <hr />
-          </>
+          </li>
         ))}
       </ul>
     </>

c9193c4c14a7f739c86901d653e7af88297d9eee
CTZDev <carlosteranzavaleta@hotmail.com>
2024-10-17 21:55:24 -0500

    (feat) Add instance Filter

--- src/components/Filter.tsx
+++ src/components/Filter.tsx
@@ -1,19 +1,28 @@
 import { Select } from "antd";
 import React from "react";
-
-const handleChange = (value: string) => {
-  console.log(`selected ${value} `);
-};
+import type { OptionSelect } from "../types/Select";
 
 interface FilterProps {
-  data: Array<{ value: string; label: string }>;
+  title: string;
+  data: Array<OptionSelect>;
 }
 
-export const Filter: React.FC<FilterProps> = ({ data }) => (
-  <Select
-    defaultValue="Seleccione"
-    style={{ width: 120 }}
-    onChange={handleChange}
-    options={data}
-  />
-);
+export const Filter: React.FC<FilterProps> = ({ title, data }) => {
+  //!TODO: ACTUALIZAR CHANGE CON VALORES.
+  const handleChange = (value: string) => {
+    console.log(`Selected: ${value} `);
+  };
+
+  return (
+    <div className="flex flex-col gap-2">
+      <h3 className="text-center font-normal text-base">{title}</h3>
+      <Select
+        defaultValue="Seleccione"
+        style={{ width: 200, fontFamily: "Poppins" }}
+        dropdownStyle={{ fontFamily: "Poppins" }}
+        onChange={handleChange}
+        options={data}
+      />
+    </div>
+  );
+};
--- src/components/Filters.tsx
+++ src/components/Filters.tsx
@@ -1,17 +1,17 @@
 import type React from "react";
-import filterCar from "../consts/filterCar";
+import carFilter from "../consts/carFilter";
 import { Filter } from "./Filter";
 
 export const Filters: React.FC = () => {
   return (
-    <>
-      <Filter data={filterCar.Marca} />
-      <Filter data={filterCar.Anio} />
-      <Filter data={filterCar["Precio min"]} />
-      <Filter data={filterCar["Precio max"]} />
-      <Filter data={filterCar.Puertas} />
-      <Filter data={filterCar.Transmisión} />
-      <Filter data={filterCar.Color} />
-    </>
+    <div className="filter-container flex flex-wrap justify-center items-center gap-10 max-w-[1200px] m-auto">
+      <Filter title={carFilter.brand.title} data={carFilter.brand.data} />
+      <Filter title={carFilter.year.title} data={carFilter.year.data} />
+      <Filter title={carFilter.minPrice.title} data={carFilter.minPrice.data} />
+      <Filter title={carFilter.maxPrice.title} data={carFilter.maxPrice.data} />
+      <Filter title={carFilter.doors.title} data={carFilter.doors.data} />
+      <Filter title={carFilter.transmission.title} data={carFilter.transmission.data} />
+      <Filter title={carFilter.color.title} data={carFilter.color.data} />
+    </div>
   );
 };

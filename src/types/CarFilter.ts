0fd139598db211f15960b3a70e7ab9f4f7037043
CTZDev <carlosteranzavaleta@hotmail.com>
2024-10-17 21:54:13 -0500

    (feat) Update interfaces for Car

---
+++ src/types/CarFilter.ts
@@ -0,0 +1,16 @@
+import { OptionSelect } from "./Select";
+
+type CarOption = {
+  data: Array<OptionSelect>;
+  title: string;
+};
+
+export type CarFilter = {
+  brand: CarOption;
+  year: CarOption;
+  minPrice: CarOption;
+  maxPrice: CarOption;
+  doors: CarOption;
+  transmission: CarOption;
+  color: CarOption;
+};
---
+++ src/types/CarList.ts
@@ -0,0 +1,9 @@
+export type CarList = {
+  id: number;
+  brand: string;
+  year: string;
+  doors: number;
+  transmission: string;
+  price: number;
+  color: string;
+};

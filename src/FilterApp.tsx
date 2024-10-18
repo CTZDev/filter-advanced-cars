baabc6ab8fec5d7740cbf38aaaa12f1143d81ed0
CTZDev <carlosteranzavaleta@hotmail.com>
2024-10-17 21:55:06 -0500

    (feat) Add CarProvider global

--- src/FilterApp.tsx
+++ src/FilterApp.tsx
@@ -1,14 +1,17 @@
 import { Filters } from "./components/Filters";
 import { Header } from "./components/Header";
 import { Table } from "./components/Table";
+import { CarProvider } from "./context/carProvider";
 import "./FilterApp.css";
 
 function FilterApp() {
   return (
     <div className="min-h-dvh">
-      <Header title="Buscador de autos" />
-      <Filters />
-      <Table />
+      <CarProvider>
+        <Header title="Buscador de autos" />
+        <Filters />
+        <Table />
+      </CarProvider>
     </div>
   );
 }

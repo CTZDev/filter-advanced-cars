181c8e083cb8e336ae7d857cbbc4f8aae59e8aa6
CTZDev <carlosteranzavaleta@hotmail.com>
2024-10-18 21:34:30 -0500

    (feat) Add conditions for the filters according to filterKey

--- src/context/carProvider.tsx
+++ src/context/carProvider.tsx
@@ -8,18 +8,80 @@ interface CarProviderProps {
 }
 
 export const CarProvider: React.FC<CarProviderProps> = ({ children }) => {
-  const [carsFilter, setCarsFilter] = useState<CarList[]>(carList);
+  const [filteredCars, setFilteredCars] = useState(carList);
+  const [currentFilters, setCurrentFilters] = useState<Partial<CarList>>({});
 
-  // !TODO: ACTUALIZAR FUNCION
-  const updateCarsFilter = (key: string) => {
-    const filterCarsForKey = carsFilter.filter((car) => {
-      return car;
-    });
-    console.log(updateCarsFilter, key);
-    setCarsFilter(filterCarsForKey);
+  const filterCars = (filters: Partial<CarList>) => {
+    // Guardamos el estado de los filtros actuales
+    const newFilters = { ...currentFilters, ...filters };
+    let filtered = [...carList];
+
+    if (newFilters.brand) {
+      const brand = newFilters.brand.toLowerCase();
+      if (brand === "seleccione") {
+        delete newFilters.brand;
+        setFilteredCars(filtered);
+      } else {
+        filtered = filtered.filter((car) => car.brand.toLowerCase().startsWith(brand));
+      }
+    }
+
+    if (newFilters.year) {
+      const year = newFilters.year;
+      if (year === "Seleccione") {
+        delete newFilters.year;
+        setFilteredCars(filtered);
+      } else {
+        filtered = filtered.filter((car) => car.year === year);
+      }
+    }
+
+    if (newFilters.price) {
+      const price = newFilters.price;
+      if (price === "Seleccione") {
+        delete newFilters.price;
+        setFilteredCars(filtered);
+      } else {
+        // !TODO => Aqui debo comprobar el tema del max y min price.
+        console.log("");
+      }
+    }
+
+    if (newFilters.doors) {
+      const doors = newFilters.doors;
+      if (doors === "Seleccione") {
+        delete newFilters.doors;
+        setFilteredCars(filtered);
+      } else {
+        filtered = filtered.filter((car) => car.doors === doors);
+      }
+    }
+
+    if (newFilters.transmission) {
+      const transmission = newFilters.transmission.toLowerCase();
+      if (transmission === "seleccione") {
+        delete newFilters.transmission;
+        setFilteredCars(filtered);
+      } else {
+        filtered = filtered.filter((car) =>
+          car.transmission.toLowerCase().startsWith(transmission)
+        );
+      }
+    }
+
+    if (newFilters.color) {
+      const color = newFilters.color.toLowerCase();
+      if (color === "seleccione") {
+        delete newFilters.color;
+        setFilteredCars(filtered);
+      } else {
+        filtered = filtered.filter((car) => car.color === color);
+      }
+    }
+
+    setCurrentFilters(newFilters);
+    setFilteredCars(filtered);
   };
 
-  return (
-    <CarContext.Provider value={{ carsFilter, updateCarsFilter }}>{children}</CarContext.Provider>
-  );
+  return <CarContext.Provider value={{ filteredCars, filterCars }}>{children}</CarContext.Provider>;
 };

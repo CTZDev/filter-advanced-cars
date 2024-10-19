615415e23e375dfc235f1d284edb134481af59bc
CTZDev <carlosteranzavaleta@hotmail.com>
2024-10-18 21:28:59 -0500

    (feat) Add filterKey with type carList

--- src/components/Filters.tsx
+++ src/components/Filters.tsx
@@ -5,13 +5,17 @@ import { Filter } from "./Filter";
 export const Filters: React.FC = () => {
   return (
     <div className="filter-container flex flex-wrap justify-center items-center gap-10 max-w-[1200px] m-auto">
-      <Filter title={carFilter.brand.title} data={carFilter.brand.data} />
-      <Filter title={carFilter.year.title} data={carFilter.year.data} />
-      <Filter title={carFilter.minPrice.title} data={carFilter.minPrice.data} />
-      <Filter title={carFilter.maxPrice.title} data={carFilter.maxPrice.data} />
-      <Filter title={carFilter.doors.title} data={carFilter.doors.data} />
-      <Filter title={carFilter.transmission.title} data={carFilter.transmission.data} />
-      <Filter title={carFilter.color.title} data={carFilter.color.data} />
+      <Filter title={carFilter.brand.title} data={carFilter.brand.data} filterKey="brand" />
+      <Filter title={carFilter.year.title} data={carFilter.year.data} filterKey="year" />
+      <Filter title={carFilter.minPrice.title} data={carFilter.minPrice.data} filterKey="price" />
+      <Filter title={carFilter.maxPrice.title} data={carFilter.maxPrice.data} filterKey="price" />
+      <Filter title={carFilter.doors.title} data={carFilter.doors.data} filterKey="doors" />
+      <Filter
+        title={carFilter.transmission.title}
+        data={carFilter.transmission.data}
+        filterKey="transmission"
+      />
+      <Filter title={carFilter.color.title} data={carFilter.color.data} filterKey="color" />
     </div>
   );
 };

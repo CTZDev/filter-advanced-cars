1a3fad9ca6dba29effff6232d89d83c5d3077685
CTZDev <carlosteranzavaleta@hotmail.com>
2024-10-18 21:31:43 -0500

    (feat) Change names for values in createContext for CarContext

--- src/context/carContext.tsx
+++ src/context/carContext.tsx
@@ -2,11 +2,11 @@ import { createContext } from "react";
 import { type CarList } from "../types/CarList";
 
 interface CarContextProps {
-  carsFilter: CarList[];
-  updateCarsFilter: (key: string) => void;
+  filteredCars: CarList[];
+  filterCars: (filters: Partial<CarList>) => void;
 }
 
 export const CarContext = createContext<CarContextProps>({
-  carsFilter: [],
-  updateCarsFilter: () => {},
+  filteredCars: [],
+  filterCars: () => {},
 });

92b3f20d1b66ce38e4706983f15ac5dac352a6d8
CTZDev <carlosteranzavaleta@hotmail.com>
2024-10-22 21:21:23 -0500

    (feat) Add exception for type of particlesjs

---
+++ src/particlesjs.d.ts
@@ -0,0 +1,5 @@
+declare module "particlesjs" {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  const particlesjs: any; // You can also replace `any` with a more specific type if you know the structure.
+  export default particlesjs;
+}

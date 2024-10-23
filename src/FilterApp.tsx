7488a71e8186c485d934eed54e0c053ad716f453
CTZDev <carlosteranzavaleta@hotmail.com>
2024-10-22 21:20:52 -0500

    (feat) Add styles for waves and executed Particles

--- src/FilterApp.tsx
+++ src/FilterApp.tsx
@@ -1,19 +1,34 @@
+import Particles from "particlesjs";
+import { useEffect } from "react";
 import { Filters } from "./components/Filters";
 import { Header } from "./components/Header";
 import { Table } from "./components/Table";
 import { CarProvider } from "./context/carProvider";
 import "./FilterApp.css";
 
-function FilterApp() {
+const FilterApp: React.FC = () => {
+  useEffect(() => {
+    Particles.init({
+      selector: ".background",
+      maxParticles: 1200,
+      color: "#0099ff",
+      speed: 0.4,
+      sizeVariations: 5,
+    });
+  }, []);
+
   return (
-    <div className="min-h-dvh">
-      <CarProvider>
-        <Header title="Buscador de autos" />
-        <Filters />
-        <Table />
-      </CarProvider>
-    </div>
+    <>
+      <canvas className="background"></canvas>
+      <div className="min-h-dvh" id="bg-wave">
+        <CarProvider>
+          <Header title="Buscador de autos" />
+          <Filters />
+          <Table />
+        </CarProvider>
+      </div>
+    </>
   );
-}
+};
 
 export default FilterApp;
